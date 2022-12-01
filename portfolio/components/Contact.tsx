aa296fa0ed905bfcee47e4c194760a6429a6e294
Alaaddine TABET <alta.tabet@gmail.com>
2022-12-01 12:32:36 +0100

    fix

--- portfolio/components/Contact.tsx
+++ portfolio/components/Contact.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import {PhoneIcon, EnvelopeIcon, MapPinIcon} from '@heroicons/react/24/solid'
+import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/solid'
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
@@ -7,10 +7,10 @@ type Inputs = {
     email: string,
     sujet: string,
     message: string
-  };
+};
 
 function Contact(): ReactElement {
-    const { register, handleSubmit} = useForm<Inputs>();
+    const { register, handleSubmit } = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = data => {
         window.location.href = `mailto:alae.tabet02@gmail.com?subject={}&body=Hi, my name is ${data.nom}. ${data.message} . ( ${data.email})
         }`
@@ -22,56 +22,64 @@ function Contact(): ReactElement {
             </h3>
 
             <div className="flex flex-col space-y-10">
-                <h4 className="text-4xl font-semibold text-center">
-                    Vous méritez plus qu'une simple application Web . Vous méritez une application Web <span className="decoration-[#F7AB0A]/50 underline">rapide </span> et 
+                <h4 className="text-2xl font-light text-center mt-16">
+                    Vous méritez plus qu'une simple application Web . Vous méritez une application Web <span className="decoration-[#F7AB0A]/50 underline">rapide </span> et
                     <span className="decoration-[#F7AB0A]/50 underline"> fiable</span> . Vous méritez un <span className="decoration-[#F7AB0A]/50 underline">développeur</span> qui peut fournir exactement ce que vous méritez.{" "}
                 </h4>
+                <div className="flex pt-12">
 
-                <div className="space-y-10">
-                    <div className="flex items-center space-x-5 justify-center">
-                        <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"/>
-                        <p className="text-2xl">+212 679516776</p>
-                    </div>
-                    <div className="flex items-center space-x-5 justify-center">
-                        <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"/>
-                        <p className="text-2xl">alae.tabet02@gmail.com</p>
-                    </div>
-                    <div className="flex items-center space-x-5 justify-center">
-                        <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"/>
-                        <p className="text-2xl">Mers Sultan , Casablanca</p>
-                    </div>
+                    {/* Contact Info */}
+                    <div className="space-y-10">
+                        <div className="flex items-center  space-x-5 ">
+                            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                            <p className="text-2xl">+212 679516776</p>
+                        </div>
+                        <div className="flex items-center space-x-5 ">
+                            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                            <p className="text-2xl">alae.tabet02@gmail.com</p>
+                        </div>
+                        <div className="flex items-center space-x-5 ">
+                            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                            <p className="text-2xl">Mers Sultan , Casablanca</p>
+                        </div>
+                        </div>
+
+
+
+                        {/* formulaire  */}
+
+                        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
+                            <div className="flex space-x-2">
+                                <input
+                                    {...register('nom')}
+                                    placeholder="Nom/Prenom "
+                                    className="contactInput"
+                                    type="text"
+                                />
+                                <input
+                                    {...register('email')}
+                                    placeholder="Email"
+                                    className="contactInput"
+                                    type="text"
+                                />
+                            </div>
+                            <input
+                                {...register('sujet')}
+                                placeholder="Sujet"
+                                className="contactInput"
+                                type="text"
+                            />
+                            <textarea
+                                {...register('message')}
+                                placeholder="Message"
+                                className="contactInput"
+                                name=""
+                            />
+                            <button type="submit" className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold">Submit </button>
+
+                        </form>
+
                 </div>
-
-                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
-                <div className="flex space-x-2">
-                    <input 
-                        {...register('nom')} 
-                        placeholder="Nom/Prenom " 
-                        className="contactInput" 
-                        type="text"
-                    />
-                    <input 
-                        {...register('email')} 
-                        placeholder="Email" 
-                        className="contactInput" 
-                        type="text"
-                    />
-                </div>
-                <input 
-                    {...register('sujet')} 
-                    placeholder="Sujet" 
-                    className="contactInput" 
-                    type="text"
-                />
-                <textarea 
-                    {...register('message')} 
-                    placeholder="Message" 
-                    className="contactInput" 
-                    name="" 
-                />
-                <button type="submit" className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold">Submit </button>
-
-            </form>
             </div>
         </div>
     )
