355196480c2c13dec84940d32f60754ae17176ca
Alaaddine TABET <alta.tabet@gmail.com>
2022-12-01 12:27:26 +0100

    add the picture and update the tailwind class in the text description

--- portfolio/components/About.tsx
+++ portfolio/components/About.tsx
@@ -17,7 +17,7 @@ export default function About({}: Props): ReactElement {
                 opacity: 1
             }}
             className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About</h3>
+            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl pb-[40px]">About</h3>
             <motion.img 
                 initial={{
                     x: -200,
@@ -30,13 +30,13 @@ export default function About({}: Props): ReactElement {
                     x:0 ,
                     opacity: 1
                 }}
-                className="space-y-2 mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg 
-                md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
-                src=""
+                className="space-y-32 mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg 
+                md:w-64 md:h-95 xl:w-[350px] xl:h-[450px]"
+                src="https://alaaddine-tabet.web.app/assets/imgAlaa2.jpg"
             />
 
-            <div className="space-y-10 px-0 md:px-10">
-                <p className="text-base">
+            <div className="w-[600px] px-0 md:px-10">
+                <p className="text-base font-bold text-sm leading-loose">
                     Développeur passionné par les nouvelles technologies et l’univers du web, je suis spécialisé 
                     dans le développement web . Polyvalent, je maîtrise plusieurs langages de programmation dont
                     HTML/CSS ,JavaScript , TypeScript  sur le frontend .
